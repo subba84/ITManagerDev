0fe521500a5c9ef872be5cc3646293c9e52d7ec7
subba84 <128273110+subba84@users.noreply.github.com>
2023-10-19 19:54:04 +0530

    Add files via upload

---
+++ ITManager.MailUtility/ITManager.MailUtilityService/MailUtilityService.cs
@@ -0,0 +1,71 @@
+﻿using ITManager.Common;
+using ITManager.MailUitlityLibrary;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITManager.MailUtilityService
+{
+    public partial class MailUtilityService : ServiceBase
+    {
+
+        private System.Timers.Timer timer;
+        public MailUtilityService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            try
+            {
+
+                int interval = Convert.ToInt32(System.Configuration.ConfigurationManager.AppSettings["interval"].ToString());
+                this.timer = new System.Timers.Timer(interval);  // 30000 milliseconds = 30 seconds
+                this.timer.AutoReset = true;
+                this.timer.Elapsed += new System.Timers.ElapsedEventHandler(this.timer_Elapsed);
+                this.timer.Start();
+            }
+            catch (Exception ex)
+            {
+                Logger.LogError(ex.Message + ex.StackTrace);
+
+            }
+
+        }
+
+        private void timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            try
+            {
+                MailManager objmailmanager = new MailManager();
+                objmailmanager.readEmails();
+
+            }
+            catch (Exception ex)
+            {
+                Logger.LogError(ex.Message + ex.StackTrace);
+
+            }
+        }
+
+        protected override void OnStop()
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                Logger.LogError(ex.Message + ex.StackTrace);
+
+            }
+        }
+    }
+}
---
+++ ITManager.MailUtility/ITManager.MailUtilityService/ProjectInstaller.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ITManager.MailUtilityService
+{
+    [RunInstaller(true)]
+    public partial class ProjectInstaller : System.Configuration.Install.Installer
+    {
+        public ProjectInstaller()
+        {
+            InitializeComponent();
+        }
+
+        public override void Install(IDictionary stateSaver)
+        {
+            base.Install(stateSaver);
+
+        }
+
+        public override void Commit(IDictionary savedState)
+        {
+            base.Commit(savedState);
+        }
+    }
+}

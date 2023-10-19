42363ae6dab768310d95d9cfcef98529ff9cc55c
subba84 <128273110+subba84@users.noreply.github.com>
2023-10-19 20:12:04 +0530

    Add files via upload

---
+++ ITManager.MailUtility/ITManager.Common/Logger.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ITManager.Common
+{
+    public class Logger
+    {
+
+        public static void LogError(string error)
+        {
+            try
+            {
+                EventWaitHandle waitHandle = new EventWaitHandle(true, EventResetMode.AutoReset, "SHARED_BY_ALL_PROCESSES");
+                waitHandle.WaitOne(5000);
+                string filename = System.AppDomain.CurrentDomain.BaseDirectory + "\\" + "EventLog.txt";
+                using (StreamWriter w = File.AppendText(filename))
+                {
+                    w.WriteLine(string.Format("Date : {0} --- Event : {1} ", DateTime.Now.ToString(), error));
+                }
+                waitHandle.Set();
+            }
+            catch (Exception ex)
+            {
+                using (EventLog eventLog = new EventLog("Application"))
+                {
+                    eventLog.Source = "Application";
+                    eventLog.WriteEntry(ex.Message + ex.StackTrace, EventLogEntryType.Information, 101, 1);
+                }
+            }
+
+        }
+    }
+}
---
+++ ITManager.MailUtility/ITManager.Common/Models/ViewModels/TicketModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ITManager.Common
+{
+    public class TicketModel
+    {
+        public string Subject { get; set; }
+
+        public string Message { get; set; }
+
+        public string UserId { get; set; }
+    }
+}

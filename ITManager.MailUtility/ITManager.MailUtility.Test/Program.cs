c624353eac0ceb3ee1a8334b448e52d1c24c5c9e
subba84 <128273110+subba84@users.noreply.github.com>
2023-10-19 19:55:05 +0530

    Add files via upload

---
+++ ITManager.MailUtility/ITManager.MailUtility.Test/Program.cs
@@ -0,0 +1,19 @@
+﻿using ITManager.MailUitlityLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITManager.MailUtility.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MailManager objmanager = new MailManager();
+            objmanager.readEmails();
+            Console.ReadKey();
+        }
+    }
+}

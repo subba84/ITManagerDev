4ba4eb8d96f3821a93c6054c583f1e100e6bac06
subba84 <128273110+subba84@users.noreply.github.com>
2023-10-19 19:37:49 +0530

    Add files via upload

---
+++ ITManager.PerfMonitor/ITManager.PerfMonitor/ITManager.Common/Models/PerformanceMetrics.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITManager.Common.Models
+{
+    public class PerformanceMetrics
+    {
+        public float CpuUtilization { get; set; }
+
+        public float MemoryUtilization { get; set; }
+
+        public float DiskUtilization { get; set; }
+
+        public float NetworkUtilization { get; set; }
+    }
+}

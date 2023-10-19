c3d4e437f4c9d3efd2c37a034bddac712092e6e8
subba84 <128273110+subba84@users.noreply.github.com>
2023-10-19 21:14:50 +0530

    Add files via upload

---
+++ ITManager.Engine/ITManager.EngineService/Program.cs
@@ -0,0 +1,31 @@
+﻿using ITManager.EngineLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITManager.EngineService
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main()
+        {
+
+            EngineManager objEngineManager = new EngineManager();
+
+            objEngineManager.UpdateGroups();
+
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[]
+            {
+                new ITManagerEngineService()
+            };
+            ServiceBase.Run(ServicesToRun);
+        }
+    }
+}

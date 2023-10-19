c853503a33ef74e68fc9a9fa69501a472dd5a962
subba84 <128273110+subba84@users.noreply.github.com>
2023-10-19 19:51:53 +0530

    Add files via upload

---
+++ ITManager.MailUtility/MailService/MailService.cs
@@ -0,0 +1,76 @@
+﻿using ITManager.Common;
+using ITManager.MailUitlityLibrary;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MailService
+{
+    public partial class MailService : ServiceBase
+    {
+        private System.Timers.Timer timer;
+        public MailService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            try
+            {
+                Logger.LogError("Service Stared");
+
+                int interval = Convert.ToInt32(System.Configuration.ConfigurationManager.AppSettings["interval"].ToString());
+                this.timer = new System.Timers.Timer(interval);  // 30000 milliseconds = 30 seconds
+                this.timer.AutoReset = true;
+                this.timer.Elapsed += new System.Timers.ElapsedEventHandler(this.timer_Elapsed);
+                this.timer.Start();
+            }
+            catch (Exception ex)
+            {
+                Logger.LogError(ex.Message + ex.StackTrace);
+
+            }
+        }
+
+        private void timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            try
+            {
+                Logger.LogError("Started reading emails");
+
+                MailManager objmailmanager = new MailManager();
+                objmailmanager.readEmails();
+
+                Logger.LogError("Completed reading emails");
+
+            }
+            catch (Exception ex)
+            {
+                Logger.LogError(ex.Message + ex.StackTrace);
+
+            }
+        }
+
+        protected override void OnStop()
+        {
+            try
+            {
+                Logger.LogError("Service Stared");
+            }
+            catch (Exception ex)
+            {
+                Logger.LogError(ex.Message + ex.StackTrace);
+
+            }
+        }
+
+       
+    }
+}
---
+++ ITManager.MailUtility/MailService/ProjectInstaller.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MailService
+{
+    [RunInstaller(true)]
+    public partial class ProjectInstaller : System.Configuration.Install.Installer
+    {
+        public ProjectInstaller()
+        {
+            InitializeComponent();
+        }
+
+        public override void Commit(IDictionary savedState)
+        {
+            string targetDirectory = Context.Parameters["DP_TargetDir"];
+
+            using (EventLog eventLog = new EventLog("Application"))
+            {
+                eventLog.Source = "Application";
+                eventLog.WriteEntry(targetDirectory, EventLogEntryType.Information, 101, 1);
+            }
+
+            Process p = new Process();
+            p.StartInfo = new ProcessStartInfo(targetDirectory + "MailServiceSetupHelper.exe");
+
+            using (EventLog eventLog = new EventLog("Application"))
+            {
+                eventLog.Source = "Application";
+                eventLog.WriteEntry(p.StartInfo.FileName, EventLogEntryType.Information, 101, 1);
+            }
+
+            p.Start();
+            p.WaitForExit();
+
+            base.Commit(savedState);
+        }
+
+    }
+}

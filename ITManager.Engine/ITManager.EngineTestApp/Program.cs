f699d414419ac803aebc3becc2f0985b9e8d7dcc
subba84 <128273110+subba84@users.noreply.github.com>
2023-10-19 21:16:15 +0530

    Add files via upload

---
+++ ITManager.Engine/ITManager.EngineTestApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using ITManager.EngineLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITManager.EngineTestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EngineManager objEngineManager = new EngineManager();
+            objEngineManager.UpdateGroups();
+        }
+    }
+}

4e0417425f89a4f87a6a27dfcb49332c2ba0aa52
subba84 <128273110+subba84@users.noreply.github.com>
2023-10-19 21:01:57 +0530

    Add files via upload

---
+++ ITManager.Engine.PowerShell/ITManager.Common/HttpManager.cs
@@ -0,0 +1,69 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITManager.Common
+{
+    public class HttpManager
+    {
+        public async Task<T> GetRequest<T>(string uri)
+        {
+            
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    client.DefaultRequestHeaders.Accept.Add(
+                    new MediaTypeWithQualityHeaderValue("application/json"));
+
+                    using (HttpResponseMessage response = await client.GetAsync(uri))
+                    {
+                        response.EnsureSuccessStatusCode();
+                        string responseBody = await response.Content.ReadAsStringAsync();
+
+                        return JsonConvert.DeserializeObject<T>(responseBody);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.LogInfo(ex.Message + ex.StackTrace + "url" + uri);
+            }
+
+            return default(T);
+        }
+
+        public async Task<dynamic> PostRequest<TIn>(string uri, TIn content)
+        {
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    client.DefaultRequestHeaders.Accept.Add(
+                    new MediaTypeWithQualityHeaderValue("application/json"));
+
+                    var serialized = new StringContent(JsonConvert.SerializeObject(content), Encoding.UTF8, "application/json");
+
+                    using (HttpResponseMessage response = await client.PostAsync(uri, serialized))
+                    {
+                        response.EnsureSuccessStatusCode();
+                        string responseBody = await response.Content.ReadAsStringAsync();
+
+                        return JsonConvert.DeserializeObject<dynamic>(responseBody);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.LogInfo(ex.Message + ex.StackTrace + "url" + uri);
+            }
+
+            return default(dynamic);
+        }
+    }
+}
---
+++ ITManager.Engine.PowerShell/ITManager.Common/Logger.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+
+namespace ITManager.Common
+{
+    public static class Logger
+    {
+
+        public static void LogInfo(string message)
+        {
+            EventWaitHandle waitHandle = new EventWaitHandle(true, EventResetMode.AutoReset, "SHARED_BY_ALL_PROCESSES");
+            waitHandle.WaitOne();
+            string filename = System.AppDomain.CurrentDomain.BaseDirectory + "\\" + "EventLog.txt";
+            using (StreamWriter w = File.AppendText(filename))
+            {
+                w.WriteLine(string.Format("Date : {0} --- Event : {1} ",DateTime.Now.ToString(),message));
+            }
+            waitHandle.Set();
+        }
+
+        public static void LogError(string message)
+        {
+            EventWaitHandle waitHandle = new EventWaitHandle(true, EventResetMode.AutoReset, "SHARED_BY_ALL_PROCESSES");
+            waitHandle.WaitOne();
+            string filename = System.AppDomain.CurrentDomain.BaseDirectory + "\\" + "EventLog.txt";
+            using (StreamWriter w = File.AppendText(filename))
+            {
+                w.WriteLine(string.Format("Date : {0} --- Event : {1} ", DateTime.Now.ToString(), message));
+            }
+            waitHandle.Set();
+        }
+
+    }
+}

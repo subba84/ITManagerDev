619061ed7bcc4e299365d9b5c75abf9a4b1f2b88
subba84 <128273110+subba84@users.noreply.github.com>
2023-10-20 19:16:23 +0530

    Add files via upload

---
+++ DataRecovery/DataRecoveryServiceSetupHelper/DataRecoveryServiceInstaller.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.Linq;
+using System.Management;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+namespace DataRecoveryServiceSetupHelper
+{
+    [RunInstaller(true)]
+    public partial class DataRecoveryServiceInstaller : System.Configuration.Install.Installer
+    {
+        public DataRecoveryServiceInstaller()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnBeforeInstall(IDictionary savedState)
+        {
+            try
+            {
+                base.OnBeforeInstall(savedState);
+                ManagementObjectCollection moc;
+                ManagementClass mc;
+                string serialNumber = string.Empty;
+                mc = new ManagementClass("Win32_BaseBoard");
+
+                moc = mc.GetInstances();
+
+                foreach (ManagementObject mo in moc)
+                {
+                    serialNumber = mo.Properties["SerialNumber"].Value.ToString();
+                }
+
+                if(string.IsNullOrEmpty(serialNumber))
+                {
+                    serialNumber = Guid.NewGuid().ToString();
+                }
+
+                FormSerialNumber obj = new FormSerialNumber();
+                obj.SerailNumber = serialNumber;
+                obj.Show();
+
+                Thread.Sleep(1000000);
+
+            }
+            catch (Exception exc)
+            {
+                Context.LogMessage(exc.ToString());
+                throw;
+            }
+        }
+    }
+}
---
+++ DataRecovery/DataRecoveryServiceSetupHelper/FormSerialNumber.cs
@@ -0,0 +1,125 @@
+﻿using InventoryManager;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Management;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DataRecoveryServiceSetupHelper
+{
+    public partial class FormSerialNumber : Form
+    {
+        public string SerailNumber { get; set; }
+
+        public FormSerialNumber()
+        {
+            InitializeComponent();
+        }
+
+        private void FormSerialNumber_Load(object sender, EventArgs e)
+        {
+            try
+            {
+
+
+                string serialNumber = string.Empty;
+
+                textBox1.Hide();
+                label1.Hide();
+
+                if (!Directory.Exists(System.AppDomain.CurrentDomain.BaseDirectory + "\\logs"))
+                {
+                    Directory.CreateDirectory(System.AppDomain.CurrentDomain.BaseDirectory + "\\logs");
+                }
+
+                ManagementObjectSearcher mSearcher = new ManagementObjectSearcher("SELECT SerialNumber, SMBIOSBIOSVersion, ReleaseDate FROM Win32_BIOS");
+                ManagementObjectCollection collection = mSearcher.Get();
+                foreach (ManagementObject obj in collection)
+                {
+                    serialNumber = (string)obj["SerialNumber"];
+
+                }
+
+                if (string.IsNullOrEmpty(serialNumber))
+                {
+                    Guid objguid = Guid.NewGuid();
+                    serialNumber = objguid.ToString();
+                    File.WriteAllText(Path.GetDirectoryName(Application.ExecutablePath) + "\\SerailNumber.txt", serialNumber);
+                }
+
+
+                lblSerialNumberValue.Text = serialNumber;
+
+            }
+            catch (Exception ex)
+            {
+                using (EventLog eventLog = new EventLog("Application"))
+                {
+                    eventLog.Source = "Application";
+                    eventLog.WriteEntry(ex.Message + ex.StackTrace, EventLogEntryType.Information, 101, 1);
+                }
+            }
+            using (EventLog eventLog = new EventLog("Application"))
+            {
+                eventLog.Source = "Application";
+                eventLog.WriteEntry(Application.ExecutablePath, EventLogEntryType.Information, 101, 1);
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                if (radioButton2.Checked)
+                {
+                    File.WriteAllText(Path.GetDirectoryName(Application.ExecutablePath) + "\\WebServiceUrl.txt", textBox1.Text);
+                }
+                Cursor.Current = Cursors.WaitCursor;
+                SystemAnalyzer objanalyzer = new SystemAnalyzer();
+                objanalyzer.AnalyzeHardwareComponents();
+                Cursor.Current = Cursors.Default;
+            }
+            catch (Exception ex)
+            {
+                using (EventLog eventLog = new EventLog("Application"))
+                {
+                    eventLog.Source = "Application";
+                    eventLog.WriteEntry(ex.Message + ex.StackTrace, EventLogEntryType.Information, 101, 1);
+                }
+
+            }
+
+
+
+            Application.Exit();
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+            {
+                label1.Hide();
+                textBox1.Hide();
+
+            }
+
+
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            label1.Show();
+            textBox1.Show();
+
+        }
+    }
+}

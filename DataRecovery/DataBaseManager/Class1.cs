1dcd3c3475d172bae79a2c0cb4809e2e9caf34d9
subba84 <128273110+subba84@users.noreply.github.com>
2023-10-20 19:26:08 +0530

    Add files via upload

---
+++ DataRecovery/DataBaseManager/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBaseManager
+{
+    public class Class1
+    {
+    }
+}

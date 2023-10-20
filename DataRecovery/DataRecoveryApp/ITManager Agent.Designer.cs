d2e2e3c020a3b372833a5c09ecd7c710cf0fa5ff
subba84 <128273110+subba84@users.noreply.github.com>
2023-10-20 19:23:07 +0530

    Add files via upload

---
+++ DataRecovery/DataRecoveryApp/ITManager Agent.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace DataRecoveryApp
+{
+    partial class FrmITManagerAgent
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmITManagerAgent));
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.dgvBackupHistory = new System.Windows.Forms.DataGridView();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvBackupHistory)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "IT Manager Agent";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Margin = new System.Windows.Forms.Padding(4);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.ShowToolTips = true;
+            this.tabControl1.Size = new System.Drawing.Size(764, 451);
+            this.tabControl1.TabIndex = 0;
+            this.tabControl1.DrawItem += new System.Windows.Forms.DrawItemEventHandler(this.tabControl1_DrawItem);
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.dgvBackupHistory);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Margin = new System.Windows.Forms.Padding(4);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(4);
+            this.tabPage1.Size = new System.Drawing.Size(756, 422);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Backup History";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // dgvBackupHistory
+            // 
+            this.dgvBackupHistory.AllowUserToAddRows = false;
+            this.dgvBackupHistory.AllowUserToDeleteRows = false;
+            this.dgvBackupHistory.AllowUserToResizeColumns = false;
+            this.dgvBackupHistory.AllowUserToResizeRows = false;
+            this.dgvBackupHistory.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvBackupHistory.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dgvBackupHistory.BackgroundColor = System.Drawing.SystemColors.ActiveCaption;
+            this.dgvBackupHistory.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvBackupHistory.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvBackupHistory.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+            this.dgvBackupHistory.Location = new System.Drawing.Point(4, 4);
+            this.dgvBackupHistory.Name = "dgvBackupHistory";
+            this.dgvBackupHistory.ReadOnly = true;
+            this.dgvBackupHistory.Size = new System.Drawing.Size(748, 414);
+            this.dgvBackupHistory.TabIndex = 0;
+            // 
+            // FrmITManagerAgent
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(764, 451);
+            this.Controls.Add(this.tabControl1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "FrmITManagerAgent";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "IT Manager Agent";
+            this.Load += new System.EventHandler(this.FrmITManagerAgent_Load);
+            this.Resize += new System.EventHandler(this.FrmITManagerAgent_Resize);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvBackupHistory)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.DataGridView dgvBackupHistory;
+    }
+}
+
---
+++ DataRecovery/DataRecoveryApp/ITManager Agent.cs
@@ -0,0 +1,116 @@
+﻿using DataRecovery.Common;
+using DataRecovery.Common.Models.ViewModels;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DataRecoveryApp
+{
+    public partial class FrmITManagerAgent : Form
+    {
+        public FrmITManagerAgent()
+        {
+            InitializeComponent();
+            tabControl1.DrawItem += new DrawItemEventHandler(tabControl1_DrawItem);
+            this.FormClosing += new FormClosingEventHandler(Form1_Closing);
+
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            LoadData();
+            this.Show();
+            this.WindowState = FormWindowState.Normal;
+            
+        }
+
+        private void FrmITManagerAgent_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                this.WindowState = FormWindowState.Minimized;
+                this.ShowInTaskbar = false;
+            }
+            catch (Exception ex)
+            {
+                Logger.LogAgentAppJson(ex.Message + ex.StackTrace);
+            }
+
+        }
+
+        private void tabControl1_DrawItem(Object sender, System.Windows.Forms.DrawItemEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            Brush _textBrush;
+
+            // Get the item from the collection.
+            TabPage _tabPage = tabControl1.TabPages[e.Index];
+
+            // Get the real bounds for the tab rectangle.
+            Rectangle _tabBounds = tabControl1.GetTabRect(e.Index);
+
+            if (e.State == DrawItemState.Selected)
+            {
+
+                // Draw a different background color, and don't paint a focus rectangle.
+                _textBrush = new SolidBrush(Color.Red);
+                g.FillRectangle(Brushes.Gray, e.Bounds);
+            }
+            else
+            {
+                _textBrush = new System.Drawing.SolidBrush(e.ForeColor);
+                e.DrawBackground();
+            }
+
+            // Use our own font.
+            Font _tabFont = new Font("Arial", 10.0f, FontStyle.Bold, GraphicsUnit.Pixel);
+
+            // Draw string. Center the text.
+            StringFormat _stringFlags = new StringFormat();
+            _stringFlags.Alignment = StringAlignment.Center;
+            _stringFlags.LineAlignment = StringAlignment.Center;
+            g.DrawString(_tabPage.Text, _tabFont, _textBrush, _tabBounds, new StringFormat(_stringFlags));
+        }
+
+        private void LoadData()
+        {
+            DirectoryInfo di = new DirectoryInfo(System.AppDomain.CurrentDomain.BaseDirectory);
+            string fileName = di.Parent.FullName + "\\IT Manager Backup\\Logs\\" + "\\BackupHistory.json";
+            if (File.Exists(fileName))
+            {
+                List<BackupHistoryModel> objroot = new List<BackupHistoryModel>();
+                objroot = JsonConvert.DeserializeObject<List<BackupHistoryModel>>(File.ReadAllText(fileName)).OrderByDescending(k => k.BackupDateTime).ToList();
+
+                dgvBackupHistory.DataSource = objroot;
+                dgvBackupHistory.Columns["BackupName"].Visible = false;
+                dgvBackupHistory.Columns["BackupSize"].Visible = false;
+                dgvBackupHistory.Columns["BackupDateTime"].HeaderText = "Backup Created";
+                dgvBackupHistory.Columns["BackupSizeText"].HeaderText = "Backup Size";
+            }
+        }
+
+        private void Form1_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+            this.ShowInTaskbar = false;
+            this.Hide();
+            e.Cancel = true;
+        }
+
+        private void FrmITManagerAgent_Resize(object sender, EventArgs e)
+        {
+            if(this.WindowState == FormWindowState.Minimized)
+            {
+                this.Hide();
+            }
+        }
+    }
+}
